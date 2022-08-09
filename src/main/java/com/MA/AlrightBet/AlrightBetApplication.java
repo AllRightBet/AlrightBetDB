c873b3d22d5ddc97f3910889526b0c4386c5a923
itMeansBigMountain <affan.fareed@gmail.com>
2022-08-08 21:07:11 -0500

    Spring Init

---
+++ src/main/java/com/MA/AlrightBet/AlrightBetApplication.java
@@ -0,0 +1,13 @@
+package com.MA.AlrightBet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlrightBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlrightBetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MA/AlrightBet/AlrightBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MA.AlrightBet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlrightBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
