ab7a54928079162f1a82b08d48765f9d8ada0473
itMeansBigMountain <affan.fareed@gmail.com>
2022-08-08 21:34:44 -0500

    Add User Entity

--- src/main/java/com/MA/AlrightBet/Entity/User.java
+++ src/main/java/com/MA/AlrightBet/Entity/User.java
@@ -2,6 +2,7 @@ package com.MA.AlrightBet.Entity;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "tbl_adminUsers")
@@ -11,12 +12,112 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-
     private String email;
-
     private String authProvider;
-
     private String username;
+    private String payment_method;
+    private String profilePic_uri;
+    private String firstName;
+    private String LastName;
+    private String address;
+    private String state;
+    private String city;
+    private int zip;
+    private int age;
+    private ArrayList<Bet> bet_history;
+
+
+
+
+
+
+    public String getPayment_method() {
+        return payment_method;
+    }
+
+    public void setPayment_method(String payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public String getProfilePic_uri() {
+        return profilePic_uri;
+    }
+
+    public void setProfilePic_uri(String profilePic_uri) {
+        this.profilePic_uri = profilePic_uri;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ArrayList<Bet> getBet_history() {
+        return bet_history;
+    }
+
+    public void setBet_history(ArrayList<Bet> bet_history) {
+        this.bet_history = bet_history;
+    }
+
+
+
+
+
+
+
+
 
 
     public User( ) {
